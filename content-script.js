c89987ceb5a6791ead4c033155861435c002a8b8
MRsk3364 <mrsk3364gaming@gmail.com>
2025-11-11 20:49:35 +0500

    Create content-script.js for webpage interaction

---
+++ content-script.js
@@ -0,0 +1,12 @@
+// Content script for webpage interaction
+
+// This script will run in the context of web pages,
+// allowing interaction with page elements.
+
+// You can access the DOM, manipulate elements,
+// and listen for events here.
+
+console.log('Content script loaded');
+
+// Example: Change background color of the webpage
+document.body.style.backgroundColor = 'lightblue';
